b591afeedb1beada220891199512624f6e360edf
bong7233 <77820303+bong7233@users.noreply.github.com>
2022-03-07 23:38:12 +0900

    Fix

--- app.py
+++ app.py
@@ -28,19 +28,19 @@ def home():
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route('/login')
+@app.route('/api/profile')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-@app.route('/sign_in', methods=['POST'])
+@app.route('/api/profile/login', methods=['POST'])
 def sign_in():
     # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+    result = db.users.find_one({'id': username_receive, 'pw': pw_hash})
 
     if result is not None:
         payload = {
@@ -55,7 +55,7 @@ def sign_in():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-@app.route('/sign_up/save', methods=['POST'])
+@app.route('/api/profile/regist', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
@@ -76,10 +76,10 @@ def sign_up():
     return jsonify({'result': 'success'})
 
 
-@app.route('/sign_up/check_dup', methods=['POST'])
+@app.route('/api/profile/regist/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
+    exists = bool(db.users.find_one({"id": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
 if __name__ == '__main__':
